ee54a6182f0774ec54ddc2a85d49b0ee2c61d82e
kayky.silva <kayky.silva@01NOT0053TI>
2025-07-02 17:01:20 -0300

    Fazendo as funções de exibir todos e buscar um

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,14 +1,21 @@
 <h1>{{$heading}}</h1>
 
-@unless(count($listings) == 0)
-@foreach($listings as $listing)
+@if(isset($listings))
+    @unless(count($listings) == 0)
+        @foreach($listings as $listing)
+            <h2>{{$listing['title']}}</h2>
+
+            <p>
+                {{$listing['description']}}
+            </p>
+        @endforeach
+    @else
+        <p>No Listing founds</p>
+    @endunless
+@else
     <h2>{{$listing['title']}}</h2>
 
     <p>
         {{$listing['description']}}
     </p>
-@endforeach
-
-@else
-    <p>No Listing founds</p>
-@endunless
+@endif
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
+//Chamando todas as Listagens
+//Criando uma rota e chamando o metodo statico get
 Route::get('/', function () {
+    //chamando a view listenings
     return view('listings', [
+        //enviando os dados do heading e de todas as listagems
         'heading' => 'Latest Listings',
-        'listings' => 
+        'listings' => Listing::all()
+    ]);
+});
+
+//Chamando uma Listagem
+Route::get('/listing', function (){
+    return view('listings', [
+        'heading' => 'One Listing',
+        'listing' => Listing::find(1)
     ]);
 });
